717fe1b38a210fbf4074d4d1d8f7d7efb4e89799
Bytekeeper <therealdante@gmail.com>
2019-07-30 10:59:32 +0200

    Gradle fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
+compileKotlin.dependsOn(tasks.generateGrammarSource)
+
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
